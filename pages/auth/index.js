a1008d064310436976cff2000bc830430af76779
Ayman Abdelazez <aymanaea52@gmail.com>
2021-02-23 02:28:28 +0200

    first commit

---
+++ pages/auth/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const authIndexPage = () => {
+    return (
+        <div>
+            <h1>Hello the auth index page</h1>
+        </div>
+    )
+}
+
+export default authIndexPage
